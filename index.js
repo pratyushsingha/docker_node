4b56d8dd183a046cb09e25a3057d9eac88b295d6
pratyush Singha <pratyushsingha83@gmail.com>
2025-07-13 12:14:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
+app.get("/health", (req, res) => {
   res.status(200).json({ status: "ok", message: "Server is healthy 🚀" });
 });
 
